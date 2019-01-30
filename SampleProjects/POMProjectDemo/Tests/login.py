d6b5186f5554b5da626068b5ac57620bcc4e1916
Jerry Kim Gomez <jerry.kim.gomez@icloud.com>
2019-01-30 04:50:29 -0800

    First Selenium POM Project

---
+++ SampleProjects/POMProjectDemo/Tests/login.py
@@ -0,0 +1,62 @@
+from selenium import webdriver
+from SampleProjects.POMProjectDemo.Pages.loginPage import LoginPage
+from SampleProjects.POMProjectDemo.Pages.homePage import HomePage
+
+import HtmlTestRunner
+import time
+import unittest
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "...", ".."))
+
+class LoginTest(unittest.TestCase):
+
+
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Firefox(executable_path="/Users/mactastic/Documents/GitHub/Selenium/drivers/geckodriver")
+        cls.driver.implicitly_wait(10)
+        cls.driver.maximize_window()
+
+    def test_01_login_valid(self):
+        driver = self.driver
+
+        driver.get("https://opensource-demo.orangehrmlive.com/")
+
+        login = LoginPage(driver)
+        login.enter_username("Admin")
+        login.enter_password("admin123")
+        login.click_login()
+
+        homepage = HomePage(driver)
+        homepage.click_welcome()
+        homepage.click_logout()
+
+        # time.sleep(2)
+
+    def test_02_login_invalid_username(self):
+        driver = self.driver
+
+        driver.get("https://opensource-demo.orangehrmlive.com/")
+
+        login = LoginPage(driver)
+        login.enter_username("Admin1")
+        login.enter_password("admin123")
+        login.click_login()
+        message = driver.find_element_by_xpath("//*[@id='spanMessage']").text
+        self.assertEqual(message, "Invalid credentials123")
+
+        time.sleep(3)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.close()
+        cls.driver.quit()
+
+        print("Test completed.")
+
+
+if __name__ == "__main__":
+    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output="/Users/mactastic/Documents/GitHub/Selenium/reports"))
+
